38fc07f6020ea6c58d0b0b53cd57cdce8774281e
nm8445 <njmais@outlook.com>
2025-02-01 22:20:46 -0500

    i hate life

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("../jobs");
+     // Set Handlebars and Markdown as template formats
+     eleventyConfig.setTemplateFormats(["md", "njk"]);
   };
